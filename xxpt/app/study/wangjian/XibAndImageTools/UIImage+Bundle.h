55923a8191f30ddfc6036e3ce29f92626d4bbe9a
wangjian <wangjian@a5a645d1-0265-4725-99e2-454b562c9f02>
2018-05-23 09:32:14 +0000

    模块化加载Xib和Image的工具

---
+++ xxpt/app/study/wangjian/XibAndImageTools/UIImage+Bundle.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+Bundle.h
+//  Pods
+//
+//  Created by linghang on 2018/4/17.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSDateFormatter+Category.h"
+
+@interface UIImage (Bundle)
++ (instancetype)ff_imagePathWithName:(NSString *)imageName bundle:(NSString *)bundle targetClass:(Class)targetClass;
+
+@end
